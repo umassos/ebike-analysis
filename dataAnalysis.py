1d8ae056593a72fd435464e0ec6b441fdfba158b
adamlechowicz <adamlechowicz921@gmail.com>
2023-02-13 12:29:48 -0500

    edits

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -21,23 +21,25 @@ for index, row in stations.iterrows():
     long = longs[index]
     points.append( (lat, long) )
 
-# DATA_DIR = "/nfs/obelix/raid2/alechowicz/valleybike/*.csv"
-# DATA_FILES = glob.glob(DATA_DIR)
+DATA_DIR = "/nfs/obelix/raid2/alechowicz/valleybike/*.csv"
+DATA_FILES = glob.glob(DATA_DIR)
 
-# # load route data
-# dfs = []
-# for file in DATA_FILES:
-#     print(file)
-#     dfs.append(pd.read_csv(file))
+# load route data
+dfs = []
+for file in DATA_FILES:
+    print(file)
+    df = pd.read_csv(file)
+    df = df.drop(['User ID'], axis=1, errors='ignore')
+    dfs.append(df)
 
-# df = pd.concat(dfs)
+df = pd.concat(dfs)
 
-# df["Date"] = pd.to_datetime(df["Date"])
-# df.sort_values(by='Date')
+df["Date"] = pd.to_datetime(df["Date"])
+df.sort_values(by='Date')
 
-print("Loading from pickle file!")
-# pickle.dump( df, open( "/nfs/obelix/raid2/alechowicz/VB.pickle", "wb" ) )
-df = pickle.load( open( "/nfs/obelix/raid2/alechowicz/VB.pickle", "rb" ) )
+# print("Loading from pickle file!")
+pickle.dump( df, open( "/nfs/obelix/raid2/alechowicz/VB.pickle", "wb" ) )
+# df = pickle.load( open( "/nfs/obelix/raid2/alechowicz/VB.pickle", "rb" ) )
 
 # locsDict = {}
 # for index, row in df.iterrows():
@@ -53,31 +55,31 @@ df = pickle.load( open( "/nfs/obelix/raid2/alechowicz/VB.pickle", "rb" ) )
 #         except:
 #             sys.stdout.write("{}, {}".format(row['Latitude'], row['Longitude']))
 
-# routesDict = {}
+routesDict = {}
 
-# for index, row in df.iterrows():
-#     ID = row['Route ID']
-#     if pd.isnull(row['Route ID']):
-#         continue
-#     if len(ID) < 10:
-#         continue
-#     location = (row['Latitude'], row['Longitude'])
-#     if location[0] > 40 and location[1] < -65:
-#         try:
-#             time = row['Date']
-#             if ID not in routesDict.keys():
-#                 routesDict[ID] = {"locations": [location], "startTime": time, "endTime": time}
-#             else:
-#                 routesDict[ID]["locations"].append(location)
-#                 routesDict[ID]["endTime"] = time
-#             sys.stdout.write(".")
-#         except:
-#             sys.stdout.write("{}, {}".format(row['Latitude'], row['Longitude']))
+for index, row in df.iterrows():
+    ID = row['Route ID']
+    if pd.isnull(row['Route ID']):
+        continue
+    if len(ID) < 10:
+        continue
+    location = (row['Latitude'], row['Longitude'])
+    if location[0] > 40 and location[1] < -65:
+        try:
+            time = row['Date']
+            if ID not in routesDict.keys():
+                routesDict[ID] = {"locations": [location], "startTime": time, "endTime": time}
+            else:
+                routesDict[ID]["locations"].append(location)
+                routesDict[ID]["endTime"] = time
+            sys.stdout.write(".")
+        except:
+            sys.stdout.write("{}, {}".format(row['Latitude'], row['Longitude']))
 
-# print("Dumping to pickle file!")
-# pickle.dump( routesDict, open( "/nfs/obelix/raid2/alechowicz/routesDict.pickle", "wb" ) )
-print("Loading from pickle file!")
-routesDict = pickle.load( open( "/nfs/obelix/raid2/alechowicz/routesDict.pickle", "rb" ) )
+print("Dumping to pickle file!")
+pickle.dump( routesDict, open( "/nfs/obelix/raid2/alechowicz/routesDict.pickle", "wb" ) )
+# print("Loading from pickle file!")
+# routesDict = pickle.load( open( "/nfs/obelix/raid2/alechowicz/routesDict.pickle", "rb" ) )
 
 durations = []
 startLocations = []
